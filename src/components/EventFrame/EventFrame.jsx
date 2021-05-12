8e22c2f40acd72b99d89de4167a276d0dabeca9d
Evyn Newton <newton.evyn@gmail.com>
2021-05-12 10:46:11 -0400

    clearing warnings

--- src/components/EventFrame/EventFrame.jsx
+++ src/components/EventFrame/EventFrame.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core";
 import VimeoFrame from "../../components/VimeoFrame/VimeoFrame";
 import backgroundimg from "../../assets/framebackground.png";
